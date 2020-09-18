e11a0cbfa8ee3c9dcd9b5a6d176183e182a8d95f
Saiyara_Oishi <saiyara.oishi@gmail.com>
2020-09-19 04:06:25 +0600

    #5 #6 #9 #12

--- solutions/47-saiyaraoishi/src/Controller.java
+++ solutions/47-saiyaraoishi/src/Controller.java
@@ -2,8 +2,7 @@ import javafx.scene.control.Button;
 
 
 public class Controller {
-    public Button button1,button2,button3,button4,button5,button6,button7,button8,button9,startWithRandomAI,startWithDefensiveAI;
-    public
+    public Button button1,button2,button3,button4,button5,button6,button7,button8,button9;
     Button button[]= new Button[10];
 
     public void initialize(){
@@ -19,60 +18,160 @@ public class Controller {
     }
 
     int count=0;
+    int[] input={-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
 
     public void click_1(){
-    if(count%2==0)
+    if(count%2==0) {
         button1.setText("X");
-    else button1.setText("O");
+        input[1]=1;
+    }
+    else if (count%2==1){
+        button1.setText("O");
+        input[1]=0;
+    }
     count++;
+    rand();
     }
     public void click_2(){
-        if(count%2==0)
+        if(count%2==0) {
             button2.setText("X");
-        else button2.setText("O");
+            input[2]=1;
+        }
+        else if (count%2==1){
+            button2.setText("O");
+            input[2]=0;
+        }
         count++;
+        rand();
     }
     public void click_3(){
-        if(count%2==0)
+        if(count%2==0) {
             button3.setText("X");
-        else button3.setText("O");
+            input[3]=1;
+        }
+        else if (count%2==1){
+            button3.setText("O");
+            input[3]=0;
+        }
         count++;
+        rand();
     }
     public void click_4(){
-        if(count%2==0)
+        if(count%2==0) {
             button4.setText("X");
-        else button4.setText("O");
+            input[4]=1;
+        }
+        else if (count%2==1){
+            button4.setText("O");
+            input[4]=0;
+        }
         count++;
+        rand();
     }
     public void click_5(){
-        if(count%2==0)
+        if(count%2==0) {
             button5.setText("X");
-        else button5.setText("O");
+            input[5]=1;
+        }
+        else if (count%2==1){
+            button5.setText("O");
+            input[5]=0;
+        }
         count++;
+        rand();
     }
     public void click_6(){
-        if(count%2==0)
+        if(count%2==0) {
             button6.setText("X");
-        else button6.setText("O");
+            input[6]=1;
+        }
+        else if (count%2==1){
+            button6.setText("O");
+            input[6]=0;
+        }
         count++;
+        rand();
     }
     public void click_7(){
-        if(count%2==0)
+        if(count%2==0) {
             button7.setText("X");
-        else button7.setText("O");
+            input[7]=1;
+        }
+        else if (count%2==1){
+            button7.setText("O");
+            input[7]=0;
+        }
         count++;
+        rand();
     }
     public void click_8(){
-        if(count%2==0)
+        if(count%2==0) {
             button8.setText("X");
-        else button8.setText("O");
+            input[8]=1;
+        }
+        else if (count%2==1){
+            button8.setText("O");
+            input[8]=0;
+        }
         count++;
+        rand();
     }
     public void click_9(){
-        if(count%2==0)
+        if(count%2==0) {
             button9.setText("X");
-        else button9.setText("O");
+            input[9]=1;
+        }
+        else if (count%2==1){
+            button9.setText("O");
+            input[9]=0;
+        }
         count++;
+        rand();
+    }
+
+    public void rand(){
+        while(count<9 && count%2==1){
+            int r= (int)(Math.random()*9)+1;
+            if(input[r]==-1){
+                if(r==1){
+                    click_1();
+                    break;
+                }
+                if (r==2){
+                   click_2();
+                   break;
+                }
+                if(r==3){
+                    click_3();
+                    break;
+                }
+                if(r==4){
+                    click_4();
+                    break;
+                }
+                if(r==5){
+                    click_5();
+                    break;
+                }
+                if(r==6){
+                    click_6();
+                    break;
+                }
+                if(r==7){
+                    click_7();
+                    break;
+                }
+                if(r==8){
+                    click_8();
+                    break;
+                }
+                if(r==9){
+                    click_9();
+                    break;
+                }
+            }
+
+        }
     }
 
 }
